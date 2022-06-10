20992f9709f279b638df2f8a0611d591e462baf7
Max Stephan <max.stephan@mni.thm.de>
2022-06-10 10:51:50 +0200

    [graph_types] added tests

--- src/data_structure/graph_types.rs
+++ src/data_structure/graph_types.rs
@@ -1,7 +1,6 @@
-#[allow(dead_code)]
 pub type Vertex = u32;
 
-#[allow(dead_code)]
+#[derive(Clone, Copy, PartialEq, Eq, Debug)]
 pub struct Dart {
     from: Vertex,
     to: Vertex,
@@ -14,7 +13,7 @@ impl Dart {
     }
 }
 
-#[allow(dead_code)]
+#[derive(Clone, Copy, PartialEq, Eq, Debug)]
 pub struct Edge {
     d1: Dart,
     d2: Dart,
@@ -33,3 +32,51 @@ impl Edge {
 
 #[allow(dead_code)]
 pub type Face = u32; // Identifier for a face.
+
+#[cfg(test)]
+mod tests {
+    use crate::data_structure::graph_types::{Dart, Edge};
+
+    #[test]
+    fn create_dart() {
+        let result = Dart::new(1, 2);
+
+        assert_eq!(result, Dart { from: 1, to: 2 });
+    }
+
+    #[test]
+    fn create_edge() {
+        let d1 = Dart::new(1, 2);
+        let d2 = Dart::new(2, 1);
+        let result = Edge::new(d1, d2);
+
+        assert_eq!(result, Some(Edge { d1, d2 }));
+    }
+
+    #[test]
+    fn create_edge_invalid() {
+        let d1 = Dart::new(10, 20);
+        let d2 = Dart::new(5, 15);
+        let result = Edge::new(d1, d2);
+
+        assert_eq!(result, None);
+    }
+
+    #[test]
+    fn create_edge_invalid_to() {
+        let d1 = Dart::new(1, 2);
+        let d2 = Dart::new(2, 5);
+        let result = Edge::new(d1, d2);
+
+        assert_eq!(result, None);
+    }
+
+    #[test]
+    fn create_edge_invalid_from() {
+        let d1 = Dart::new(1, 2);
+        let d2 = Dart::new(5, 1);
+        let result = Edge::new(d1, d2);
+
+        assert_eq!(result, None);
+    }
+}
