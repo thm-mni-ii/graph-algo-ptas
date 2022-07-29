d29bff9301a2bc2e788d10f1f3f919f3697ee64e
Pia-Doreen Ritzke <pia-doreen.ritzke@mni.thm.de>
2022-07-29 12:31:33 +0200

    Fixes bug in tree decomposition generation and adds test cases.

--- src/algorithm/tree_decomposition.rs
+++ src/algorithm/tree_decomposition.rs
@@ -18,7 +18,7 @@ fn tree_decomposition(
         LinkGraphIter<LinkFace>,
     >,
     dual_graph: HashMap<LinkFace, HashSet<LinkFace>>,
-    spantree: HashMap<LinkVertex, LinkVertex>,
+    spantree: &HashMap<LinkVertex, LinkVertex>,
     root_vertex: LinkFace,
 ) -> TreeDecomposition {
     let mut tree: TreeDecomposition = Default::default();
@@ -55,7 +55,7 @@ fn add_bags(
         tree.add_child_bags(parent, vec![bag]);
     }
 
-    for c in dual_graph.get(&vertex).unwrap() {
+    for c in dual_graph.get(&vertex).unwrap_or(&HashSet::new()) {
         add_bags(
             c.clone(),
             vertex.get_id(),
@@ -103,4 +103,103 @@ fn get_face_vertices(
         dart = graph.next(&dart);
     }
     result
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::algorithm::dualgraph::dual_graph;
+    use crate::algorithm::spantree::span;
+    use crate::algorithm::tree_decomposition::tree_decomposition;
+    use crate::data_structure::link_graph::LinkGraph;
+    use fxhash::FxHashSet;
+
+    #[test]
+    fn single_edge() {
+        let mut lg = LinkGraph::new();
+        let lv1 = lg.new_vertex();
+        let lv2 = lg.new_vertex();
+
+        let ld1 = lg.new_dart(lv1.clone(), lv2.clone(), None, None, None, None);
+        let lf = lg.new_face(ld1.clone());
+        lg.new_dart(
+            lv2.clone(),
+            lv1.clone(),
+            Some(ld1.clone()),
+            Some(ld1.clone()),
+            Some(ld1.clone()),
+            Some(lf.clone()),
+        );
+        let span = span(&lg, lv1.clone());
+        let td = tree_decomposition(&lg, dual_graph(&lg, &span), &span, lf);
+
+        println!("[RESULT]: {:?}", td);
+
+        assert_eq!(td.bags.len(), 1);
+        assert_eq!(td.max_bag_size, 2);
+        assert_eq!(td.bags[0].vertex_set.len(), 2);
+        let mut cb = FxHashSet::default();
+        cb.insert(lv1.get_id());
+        cb.insert(lv2.get_id());
+        assert_eq!(td.bags[0].vertex_set, cb)
+    }
+
+    #[test]
+    fn triangle() {
+        let mut lg = LinkGraph::new();
+        let lv0 = lg.new_vertex();
+        let lv1 = lg.new_vertex();
+        let lv2 = lg.new_vertex();
+
+        let ld0 = lg.new_dart(lv0.clone(), lv1.clone(), None, None, None, None);
+        let lf = lg.new_face(ld0.clone());
+        let ld1 = lg.new_dart(
+            lv1.clone(),
+            lv2.clone(),
+            Some(ld0.clone()),
+            None,
+            None,
+            Some(lf.clone()),
+        );
+        let ld2 = lg.new_dart(
+            lv2.clone(),
+            lv0.clone(),
+            Some(ld1.clone()),
+            Some(ld0.clone()),
+            None,
+            Some(lf.clone()),
+        );
+
+        let lt0 = lg.new_dart(lv1.clone(), lv0.clone(), None, None, Some(ld0), None);
+        let lof = lg.new_face(lt0.clone());
+        let lt2 = lg.new_dart(
+            lv0.clone(),
+            lv2.clone(),
+            Some(lt0.clone()),
+            None,
+            Some(ld2),
+            Some(lof.clone()),
+        );
+        lg.new_dart(
+            lv2.clone(),
+            lv1.clone(),
+            Some(lt2),
+            Some(lt0),
+            Some(ld1),
+            Some(lof.clone()),
+        );
+
+        let span = span(&lg, lv1.clone());
+        let td = tree_decomposition(&lg, dual_graph(&lg, &span), &span, lof);
+
+        println!("[RESULT]: {:?}", td);
+
+        assert_eq!(td.bags.len(), 1);
+        assert_eq!(td.max_bag_size, 3);
+        assert_eq!(td.bags[0].vertex_set.len(), 3);
+        let mut cb = FxHashSet::default();
+        cb.insert(lv1.get_id());
+        cb.insert(lv0.get_id());
+        cb.insert(lv2.get_id());
+        assert_eq!(td.bags[0].vertex_set, cb)
+    }
 }
