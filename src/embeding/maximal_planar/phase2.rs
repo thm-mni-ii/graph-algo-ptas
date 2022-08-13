9e24bbb02ebabbbb9075be0c26b05e5680aa2f21
Max Stephan <max.stephan@mni.thm.de>
2022-08-13 14:58:36 +0200

    [pahase2] fix face creation

--- src/embeding/maximal_planar/phase2.rs
+++ src/embeding/maximal_planar/phase2.rs
@@ -1,4 +1,4 @@
-use crate::data_structure::link_graph::{LinkGraph, LinkVertex};
+use crate::data_structure::link_graph::{LinkDart, LinkGraph, LinkVertex};
 
 pub struct Phase2<'a> {
     dcel: &'a mut LinkGraph,
@@ -15,28 +15,41 @@ impl Phase2<'_> {
         let v2 = self.dcel.new_vertex();
         let v3 = self.dcel.new_vertex();
 
-        self.create_face(v0.clone(), v1.clone(), v3.clone());
-        self.create_face(v1, v2.clone(), v3.clone());
-        self.create_face(v2, v0, v3);
+        // Face 0
+        let (d0, d1, d2) = self.create_face(v0.clone(), v1.clone(), v3.clone(), None, None, None);
+
+        // Face 1
+        let (_, d3, d4) =
+            self.create_face(v1.clone(), v0.clone(), v2.clone(), Some(d0), None, None);
+
+        // Face 2
+        let (d5, _, _) = self.create_face(v3.clone(), v2.clone(), v0, None, Some(d3), Some(d2));
+
+        // Face 3
+        self.create_face(v2, v3, v1, Some(d5), Some(d1), Some(d4));
     }
 
-    fn create_face(&mut self, vertex1: LinkVertex, vertex2: LinkVertex, vertex3: LinkVertex) {
-        let (d0, t0) = self
+    fn create_face(
+        &mut self,
+        v0: LinkVertex,
+        v1: LinkVertex,
+        v2: LinkVertex,
+        t1: Option<LinkDart>,
+        t2: Option<LinkDart>,
+        t3: Option<LinkDart>,
+    ) -> (LinkDart, LinkDart, LinkDart) {
+        let d0 = self
             .dcel
-            .new_edge(vertex1.clone(), vertex2.clone(), None, None, None, None);
+            .new_dart(v0.clone(), v1.clone(), None, None, t1, None);
         let f0 = self.dcel.new_face(d0.clone());
-        let tf0 = self.dcel.new_face(t0.clone()); // ToDo macht das semantisch sinn?
-        let (d1, _t1) = self.dcel.new_edge(
-            vertex2,
-            vertex3.clone(),
-            Some(d0.clone()),
-            None,
-            Some(f0.clone()),
-            Some(tf0.clone()),
-        );
-        let (_d2, _t2) =
-            self.dcel
-                .new_edge(vertex3, vertex1, Some(d1), Some(d0), Some(f0), Some(tf0));
+        let d1 = self
+            .dcel
+            .new_dart(v1, v2.clone(), Some(d0.clone()), None, t2, Some(f0.clone()));
+        let d2 = self
+            .dcel
+            .new_dart(v2, v0, Some(d1.clone()), Some(d0.clone()), t3, Some(f0));
+
+        (d0, d1, d2)
     }
 }
 
@@ -52,8 +65,9 @@ mod tests {
 
         Phase2::new(&mut dcel).execute();
 
+        dcel.validate();
         assert_eq!(dcel.get_vertexes().count(), 4);
-        assert_eq!(dcel.get_faces().count(), 3);
-        // TODO: Test structure
+        assert_eq!(dcel.edge_count(), 6);
+        assert_eq!(dcel.get_faces().count(), 4);
     }
 }
