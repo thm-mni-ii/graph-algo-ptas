05e372187e284117cff7c114961230eb4786c92e
Max Stephan <max.stephan@mni.thm.de>
2022-08-16 20:35:26 +0200

    [dot_reader] added some tests

--- src/data_structure/dot_reader.rs
+++ src/data_structure/dot_reader.rs
@@ -58,3 +58,45 @@ fn get_or_create_node(
         .entry(node)
         .or_insert_with(|| graph.add_node(()))
 }
+
+#[cfg(test)]
+mod tests {
+    use super::read_graph;
+
+    #[test]
+    fn small_graph() {
+        let input = "graph g { 0[]\n 1[]\n 2[]\n 3[]\n 0 -- 1\n 2 --3\n 2 -- 1 }";
+        let g = read_graph(input.to_string());
+
+        assert!(g.is_some());
+        let graph = g.unwrap();
+        assert_eq!(graph.node_count(), 4);
+        assert_eq!(graph.edge_count(), 3);
+    }
+
+    #[test]
+    fn small_graph_no_nodes() {
+        let input = "graph g { 0 -- 1\n 2 --3\n 2 -- 1\n 10 -- 20\n 4 -- 1\n 5 -- 3 }";
+        let g = read_graph(input.to_string());
+
+        assert!(g.is_some());
+        let graph = g.unwrap();
+        assert_eq!(graph.node_count(), 8);
+        assert_eq!(graph.edge_count(), 6);
+    }
+
+    #[test]
+    fn digraph() {
+        let input = "digraph g { 0 -- 1 }";
+        let g = read_graph(input.to_string());
+
+        assert!(g.is_none());
+    }
+
+    #[test]
+    #[should_panic]
+    fn invalid_graph() {
+        let input = "graph { 0 -- 1 }";
+        read_graph(input.to_string());
+    }
+}
