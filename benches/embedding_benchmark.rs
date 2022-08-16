03714001c1459be2d5261cd5762f86aae2d05d81
Jonas-Ian Kuche <jonas-ian.kuche@mni.thm.de>
2022-08-16 14:20:38 +0200

    feat: improve embeding benchmark

--- benches/embedding_benchmark.rs
+++ benches/embedding_benchmark.rs
@@ -1,25 +1,33 @@
-use criterion::{criterion_group, criterion_main, Criterion};
+use criterion::{criterion_group, criterion_main, Criterion, black_box, Bencher};
 use graph_algo_ptas::embedding::index::Embedding;
 use graph_algo_ptas::embedding::maximal_planar::index::MaximalPlanar;
 use graph_algo_ptas::generation::planar::generate;
+use std::time::Duration;
 
-fn embedding_benchmark(c: &mut Criterion) {
-    let mut i = 0;
-    c.bench_function("embed", |b| {
+fn bench_embedding(n: usize) -> impl Fn(&mut Bencher) {
+    move |b| {
+        let graph = generate(
+            n,
+            Some(44)
+        ).to_pet_graph();
         b.iter(|| {
             MaximalPlanar::embed(
-                generate(
-                    10,
-                    Some({
-                        i += 1;
-                        i
-                    }),
-                )
-                .to_pet_graph(),
+                black_box(graph.clone())
             )
         })
-    });
+    }
 }
 
-criterion_group!(benches, embedding_benchmark);
+fn embedding_benchmark(c: &mut Criterion) {
+    let mut g = c.benchmark_group("MaximalPlanar embedding");
+    g.bench_function("MaximalPlanar embedding (|G|=10)", bench_embedding(10));
+    g.bench_function("MaximalPlanar embedding (|G|=100)", bench_embedding(100));
+    g.bench_function("MaximalPlanar embedding (|G|=1000)", bench_embedding(1000));
+}
+
+criterion_group!{
+    name = benches;
+    config = Criterion::default().measurement_time(Duration::from_secs(10));
+    targets = embedding_benchmark
+}
 criterion_main!(benches);
